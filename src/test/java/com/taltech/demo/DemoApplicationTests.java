cb3dd1d5ab5ca5e84d0e3fa099e53cd7fcb8985d
Kamal-Abdullayev <kamalabdu2layev@gmail.com>
2024-10-02 18:46:24 +0300

    test - getProductList()

--- src/test/java/com/taltech/demo/DemoApplicationTests.java
+++ src/test/java/com/taltech/demo/DemoApplicationTests.java
@@ -1,13 +1,46 @@
 package com.taltech.demo;
 
-import org.junit.jupiter.api.Test;
+import com.taltech.demo.model.Product;
+import com.taltech.demo.repository.ProductRepository;
+import com.taltech.demo.service.ProductService;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private ProductService productService;
+	private ProductRepository productRepository;
+
+	@BeforeEach
+	public void setUp() {
+		productService = Mockito.mock(ProductService.class);
+		productRepository = Mockito.mock(ProductRepository.class);
 	}
+
+	@DisplayName("Should return list of products")
+	@Test
+	void shouldReturnProductList() {
+		List<Product> mockData = new ArrayList<>();
+		mockData.add(new Product("Product 1", "Test product", 10, 5));
+		mockData.add(new Product("Product 2", "Test product", 8, 3));
+		mockData.add(new Product("Product 3", "Test product", 15, 1));
+		mockData.add(new Product("Product 4", "Test product", 20, 9));
+
+		Mockito.when(productRepository.findAll()).thenReturn(mockData);
+		Mockito.when(productService.getProductList()).thenAnswer(invocation -> productRepository.findAll());
+
+		List<Product> result = productService.getProductList();
+
+		Assertions.assertEquals(4, result.size());
+		Assertions.assertEquals(mockData, result);
+	}
+
+
+
 
 }
