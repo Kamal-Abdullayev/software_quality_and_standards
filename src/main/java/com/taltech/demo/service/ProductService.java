43af753543b5218b592815b0122e3ab6c602b4c1
Kamal-Abdullayev <kamalabdu2layev@gmail.com>
2024-10-02 18:46:56 +0300

    code - getProductList()

--- src/main/java/com/taltech/demo/service/ProductService.java
+++ src/main/java/com/taltech/demo/service/ProductService.java
@@ -1,10 +1,22 @@
 package com.taltech.demo.service;
 
+import com.taltech.demo.model.Product;
+import com.taltech.demo.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+    private ProductRepository productRepository;
 
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProductList() {
+        return productRepository.findAll();
+    }
 
 
 
