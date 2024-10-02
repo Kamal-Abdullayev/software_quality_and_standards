5ec02e83224f6a8503a04d37e1821dc2ead50e49
Kamal-Abdullayev <kamalabdu2layev@gmail.com>
2024-10-02 17:54:47 +0300

    db connection

--- src/main/java/com/taltech/demo/DemoApplication.java
+++ src/main/java/com/taltech/demo/DemoApplication.java
@@ -1,10 +1,21 @@
 package com.taltech.demo;
 
+import com.taltech.demo.model.Product;
+import com.taltech.demo.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+	private ProductRepository productRepository;
+
+	public DemoApplication(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
 
 	public static void main(String[] args) {
 
@@ -13,4 +24,14 @@ public class DemoApplication {
 
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		List<Product> products = new ArrayList();
+		products.add(new Product("Product 1", "Test product", 10, 5));
+		products.add(new Product("Product 2", "Test product", 8, 3));
+		products.add(new Product("Product 3", "Test product", 15, 1));
+		products.add(new Product("Product 4", "Test product", 20, 9));
+
+		productRepository.saveAll(products);
+	}
 }
--- src/main/java/com/taltech/demo/model/Product.java
+++ src/main/java/com/taltech/demo/model/Product.java
@@ -1,21 +1,21 @@
 package com.taltech.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
     private String description;
     private double price;
     private int quantity;
 
     public Product() {
-    }
-
-    public Product(long id, String name, String description, double price, int quantity) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.quantity = quantity;
     }
 
     public Product(String name, String description, double price, int quantity) {
